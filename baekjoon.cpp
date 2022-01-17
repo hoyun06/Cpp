8708413e6c6608d11222a4892f3b6ac93d54c116
hoyun06 <hoyun06@gmail.com>
2022-01-17 23:35:27 +0900

    problem 2164

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -5,63 +5,35 @@ using namespace std;
 //cin.tie(NULL);
 //ios_base::sync_with_stdio(false);
 
-class Stack
-{
-    vector<int> v;
-public:
-    Stack() {}
-    void push(int x) { v.push_back(x); }
-    int pop()
-    {
-        if (v.size() == 0) { return -1; }
-        int tmp;
-        vector<int>::iterator it;
-        it = v.end() - 1;
-        tmp = *it;
-        it = v.erase(it);
-        return tmp;
-    }
-    int size() { return v.size(); }
-    int empty() { if (v.size() == 0) { return 1; } else { return 0; } }
-    int top()
-    {
-        if (v.size() == 0) { return -1; }
-        return v[v.size() - 1];
-    }
-};
 
 int main()
 {
-    Stack s;
+    vector<int> v;
     int n;
     cin >> n;
-    cin.ignore();
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        string tmp;
-        getline(cin, tmp);
-        if (tmp.find("push") != -1)
+        v.push_back(i);
+    }
+    int ind = 0;
+    while (true)
+    {
+        if (v.size() == 1)
         {
-            int ind = tmp.find(" ") + 1;
-            string t = tmp.substr(ind, tmp.length() - ind);
-            s.push(stoi(t));
+            cout << v[0];
+            return 0;
         }
-        else if (tmp == "pop")
+        if (ind % 2 == 0)
         {
-            cout << s.pop() << "\n";
-        }
-        else if (tmp == "size")
-        {
-            cout << s.size() << "\n";
-        }
-        else if (tmp == "empty")
-        {
-            cout << s.empty() << "\n";
+            ind++;
+            if (ind == v.size() - 1) { break; }
         }
         else
         {
-            cout << s.top() << "\n";
+            v.push_back(v[ind]);
+            ind++;
         }
     }
+    cout << v[v.size() - 1];
     return 0;
-}
+}	
